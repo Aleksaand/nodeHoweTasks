127cd80e611ea3b4d2b278aff6f574d0c48ea7b7
Aleksandr <ovcharenko_aleksandr@ukr.net>
2021-06-10 16:56:27 +0300

    first homework to git

--- 10.06.2021/main.js
+++ 10.06.2021/main.js
@@ -1,1 +1,29 @@
-console.log('HI');
+const http = require('http');
+const fs = require('fs');
+const axios = require('axios');
+
+http.createServer( async (request, response) => {
+    
+    
+
+
+    const dataaxios = await axios.get('http://randomuser.me/api/?results=5000');
+    const { data: results } = dataaxios;
+    
+    const arrayData = results.results;
+    
+    const dataToFile = JSON.stringify(arrayData);
+    console.log(typeof(dataToFile));
+    const datasplit = dataToFile.split(",")+'\n';
+    
+    fs.appendFile('file.txt', datasplit, (err) => {
+        if(err) throw err;
+        console.log('Data has been added!');
+    });
+    response.end("Hello world!");
+    const stream = fs.createWriteStream('file1.txt', 'utf-8');
+    
+    const streamRead = fs.createReadStream('file.txt', 'utf-8');
+    streamRead.pipe(stream);
+    
+}).listen(11000);
